502e055acd6956520c9bbb92f28a061a1f9cd6b8
KotaSakurai <sakurai.kota@mwed.co.jp>
2017-07-12 14:31:31 +0900

    add wercker rspec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :user do
-    name "MyString"
-    email "MyString"
-    password "MyString"
-    password_confirmation "MyString"
-    age ""
-    gender ""
+    name "KKKtest"
+    email "test@test.com"
+    password "foobar"
+    password_confirmation "foobar"
+    age 23
+    gender 1
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '#new' do
+
+    context '' do
+    end
+  end
 end
