4d656a6dcac9891c53dd79a22ca5687f11de84e1
KotaSakurai <sakurai.kota@mwed.co.jp>
2017-07-13 11:12:30 +0900

    add user cookies save controller and view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,11 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      if params[:session][:remember_me] == 1
+        remember user
+      else
+        forget user
+      end
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination' # 本当は正しくない
@@ -14,7 +19,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170712063343) do
+ActiveRecord::Schema.define(version: 20170713004720) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170712063343) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
