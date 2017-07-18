2c75c373e8539b6deab998a6d5624e57a925ee26
KotaSakurai <sakurai.kota@mwed.co.jp>
2017-07-18 11:18:37 +0900

    add reset password and make user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'sessions/new'
 
   get 'users/index'
@@ -12,5 +16,6 @@ Rails.application.routes.draw do
   get '/account_activations/:id/edit', to: 'account_activations#edit'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170713092647) do
+ActiveRecord::Schema.define(version: 20170714075446) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20170713092647) do
     t.string "activation_digest"
     t.boolean "activated"
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
 end
