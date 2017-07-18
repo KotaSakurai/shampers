2f526cbd6f2e7e3183c5d0275d1852b76b5a039e
KotaSakurai <sakurai.kota@mwed.co.jp>
2017-07-18 16:06:25 +0900

    fix update_attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,8 +38,7 @@ class User < ApplicationRecord
   end
 
   def activate
-    update_attribute(:activated, true)
-    update_attribute(:activated_at, Time.zone.now)
+    update_attributes(activated: true, activated_at: Time.zone.now )
   end
 
   def send_activation_email
@@ -48,8 +47,7 @@ class User < ApplicationRecord
 
   def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(:reset_digest, User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
+    update_attributes(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
   end
 
   def send_password_reset_email
