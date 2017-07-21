13577ffa252f7fb9db8fd95866d1651f59ba99c5
KotaSakurai <sakurai.kota@mwed.co.jp>
2017-07-21 09:29:26 +0900

    add shampoo validation

--- app/models/shampoo.rb
+++ app/models/shampoo.rb
@@ -1,4 +1,9 @@
 class Shampoo < ApplicationRecord
   mount_uploader :image, ImageUploader
   has_many :tags
+
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :storage, presence: true
+  validates :image, presence: true
 end
