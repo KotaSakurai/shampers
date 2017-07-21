7056b2d081635a275f94977c3ca2a61eb4c356ce
KotaSakurai <sakurai.kota@mwed.co.jp>
2017-07-21 09:57:52 +0900

    add spec tag model test

--- spec/factories/tags.rb
+++ spec/factories/tags.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :tag do
-    name "MyString"
+    name "good"
     shampoo_id 1
     user_id 1
   end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { tag }
+
+  describe 'create tags' do
+    let(:tag) { build(:tag, params) }
+
+
+    context 'when not presence tag' do
+      let(:params) { { name: '' } }
+
+      it { is_expected.to be_invalid }
+    end 
+
+    context 'when not valid user_id' do
+      let(:params) { { user_id: 2000 } }
+
+      it { is_expected.to be_invalid }
+    end
+
+    context 'when not valid shampoo_id' do
+      let(:params) { { shampoo_id: 2000 } }
+
+      it { is_expected.to be_invalid }
+    end
+  end 
 end
