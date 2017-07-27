0b8f1adc1acb4e340c84c7531da4700496e39aed
KotaSakurai <sakurai.kota@mwed.co.jp>
2017-07-27 19:05:03 +0900

    add tag uniquness scope

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, :uniqueness => { :scope => :shampoo_id }
   validates :shampoo_id, presence: true
   validates :user_id, presence: true
   belongs_to :user
