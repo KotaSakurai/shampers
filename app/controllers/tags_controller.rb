f7f8d208e3b0dd01a56b0bcf25b73fe040f35a3a
KotaSakurai <sakurai.kota@mwed.co.jp>
2017-11-16 22:11:34 +0900

    Delete only login function

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,7 +3,7 @@ class TagsController < ApplicationController
   end
 
   def create
-    @tag = current_user.tags.build(tag_params)
+    @tag = Tag.new(tag_params.merge(user_id: 1))
     if @tag.save
       flash[:info] = "Posted Shampoo!"
       redirect_to root_url
