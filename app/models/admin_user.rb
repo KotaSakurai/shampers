b466923b422260fe17d5378feb6068fa11b1766e
KotaSakurai <sakurai.kota@mwed.co.jp>
2018-03-08 01:09:34 +0900

    Add admin and device

---
+++ app/models/admin_user.rb
@@ -0,0 +1,8 @@
+class AdminUser < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable,
+         :rememberable, :trackable, :validatable
+
+  # attr_accessor :email, :password, :password_confirmation, :remember_me
+end
