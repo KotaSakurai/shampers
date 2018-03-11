f2aeb4e6b4df0398ac826051c5fce55a517539e8
KotaSakurai <sakurai.kota@mwed.co.jp>
2018-03-12 00:00:29 +0900

    Fix route

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  # resources :users
+  resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :shampoos
