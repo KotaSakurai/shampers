604ec09b26414037d4a67432813e70ef35f0b318
KotaSakurai <sakurai.kota@mwed.co.jp>
2018-06-29 03:51:23 +0900

    Fix auto correct

--- db/migrate/20180307144600_devise_create_admin_users.rb
+++ db/migrate/20180307144600_devise_create_admin_users.rb
@@ -32,11 +32,10 @@ class DeviseCreateAdminUsers < ActiveRecord::Migration[5.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
       t.timestamps null: false
     end
 
-    add_index :admin_users, :email,                unique: true
+    add_index :admin_users, :email, unique: true
     # add_index :admin_users, :confirmation_token,   unique: true
     # add_index :admin_users, :unlock_token,         unique: true
   end
